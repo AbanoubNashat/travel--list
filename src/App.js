f713d0f763df6dc4d56c2bc11fde18b8c1941f81
Abanoub Nashat <nashatabanop@gmail.com>
2026-09-18 15:18:38 +0300

    Add sorting functionality to PackingList component

--- src/App.js
+++ src/App.js
@@ -84,10 +84,31 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  // sorted list container
+  let sortedList;
+  if (sortBy === "input") {
+    sortedList = items;
+  }
+
+  if (sortBy === "description") {
+    // .slice() makes a copy of the list and make our operations on that copy as we can mutate the items as it's a state.
+    sortedList = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedList = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedList.map((item) => (
           <Item
             item={item}
             onToggleItem={onToggleItem}
@@ -96,6 +117,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input</option>
+          <option value="description">sort by description</option>
+          <option value="packed">sort by packed</option>
+        </select>
+      </div>
     </div>
   );
 }
